0653640e270dd243adab73482233dcd6275abd4c
Maryan <marianlisovskyi@gmail.com>
2022-12-24 23:08:24 +0200

    delet comm

--- mini-project/details.js
+++ mini-project/details.js
@@ -1,9 +1,3 @@
-// На странице user-details.html:
-// 4 Вивести всю, без виключення, інформацію про об'єкт user на який клікнули
-// 5 Додати кнопку "post of current user", при кліку на яку, з'являються title всіх постів поточного юзера
-// (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
-// 6 Каждому посту додати кнопку/посилання, при кліку на яку відбувається перехід на сторінку post-details.html, котра має детальну інфу про поточний пост.
-
 const url = new URL(location.href);
 const id = url.searchParams.get('date');
 
